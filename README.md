f542f7741e2a02ae6258a2c5a7f51f959e954613
Robert C Wu <rwu128@nam.ad.pwcinternal.com>
2018-11-22 14:15:12 +0800

    init upload code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# isFasly
+# isFalsy
+
 Detect whether a value is falsy.
+
+## Usage
+
+1. broswer
+
+```html
+<script src="./isFalsy.min.js"></script>
+```
+
+2. node
+
+```
+npm install isFalsy
+```
+
+## Reference Documents
+
+https://tc39.github.io/ecma262/#sec-toboolean
+
+https://developer.mozilla.org/en-US/docs/Glossary/Falsy
---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Detect whether a value is falsy
+ * @param {any} value
+ * @return {boolean}
+ */
+function isFalsy(value) {
+  return (
+    value === false ||
+    value === undefined ||
+    value === null ||
+    value === NaN ||
+    value === 0 ||
+    value === "" ||
+    (Object.prototype.toString.call(document) === "[object HTMLDocument]" &&
+      value === document.all)
+  );
+}
+
+if (module.exports) {
+  module.exports = isFalsy;
+}
